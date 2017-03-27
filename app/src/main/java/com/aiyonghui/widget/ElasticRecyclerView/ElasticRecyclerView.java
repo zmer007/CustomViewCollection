05d75679b3ed23be4d093357c1cfa3c87f341c6d
zmer007 <177964617@qq.com>
2017-03-27 10:12:58 +0800

    添加加载更多／刷新监听

--- app/src/main/java/com/aiyonghui/widget/ElasticRecyclerView/ElasticRecyclerView.java
+++ app/src/main/java/com/aiyonghui/widget/ElasticRecyclerView/ElasticRecyclerView.java
@@ -1,8 +1,10 @@
 package com.aiyonghui.widget.ElasticRecyclerView;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.LayoutManager;
 import android.util.AttributeSet;
 
 /**
@@ -10,18 +12,73 @@ import android.util.AttributeSet;
  * Date: 2017-03-26
  * Function: 一个有弹性的有下拉刷新和上拉加载更多功能的RecyclerView，100%与多看阅读列表相同。
  */
-public class ElasticRecyclerView extends RecyclerView {
+public class ElasticRecyclerView extends SwipeRefreshLayout {
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private RecyclerView mRecyclerView;
+
+    private OnLoadListener mLoadListener;
+
+    private boolean mLoading = false;
+
 
     public ElasticRecyclerView(Context context) {
         super(context);
+        addRecyclerView(context);
     }
 
-    public ElasticRecyclerView(Context context, @Nullable AttributeSet attrs) {
+    public ElasticRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        addRecyclerView(context);
     }
 
-    public ElasticRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    private void addRecyclerView(Context context) {
+        mRecyclerView = new RecyclerView(context);
+        mRecyclerView.setLayoutParams(new SwipeRefreshLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (mLoadListener != null && isIdle() && !ViewCompat.canScrollVertically(mRecyclerView, 1)) {
+                    mLoading = true;
+                    mLoadListener.onLoad();
+                }
+            }
+        });
+
+        addView(mRecyclerView);
     }
 
+    public void setLayoutManager(LayoutManager layoutManager) {
+        mRecyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void setAdapter(RecyclerView.Adapter adapter) {
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void setOnRefreshListener(OnRefreshListener listener) {
+        super.setOnRefreshListener(listener);
+    }
+
+    public void setOnLoadListener(OnLoadListener loadListener) {
+        mLoadListener = loadListener;
+    }
+
+    public interface OnLoadListener {
+        void onLoad();
+    }
+
+    public void setLoading(boolean loading){
+        mLoading = loading;
+    }
+
+    public boolean isIdle() {
+        return !mLoading && !isRefreshing();
+    }
+
+    public boolean isLoading() {
+        return mLoading;
+    }
 }
--- app/src/main/java/com/aiyonghui/widget/MainActivity.java
+++ app/src/main/java/com/aiyonghui/widget/MainActivity.java
@@ -1,13 +1,18 @@
 package com.aiyonghui.widget;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
 import com.aiyonghui.widget.ElasticRecyclerView.EAdapter;
 import com.aiyonghui.widget.ElasticRecyclerView.ElasticRecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     ElasticRecyclerView mElasticRecyclerView;
 
@@ -18,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
         mElasticRecyclerView = (ElasticRecyclerView) findViewById(R.id.activity_main);
         mElasticRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mElasticRecyclerView.setAdapter(new EAdapter());
+        mElasticRecyclerView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mElasticRecyclerView.setRefreshing(false);
+                    }
+                }, 3000);
+            }
+        });
+        mElasticRecyclerView.setOnLoadListener(new ElasticRecyclerView.OnLoadListener() {
+            @Override
+            public void onLoad() {
+                Log.d(TAG, "onLoad: +++++++");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mElasticRecyclerView.setLoading(false);
+                    }
+                }, 3000);
+            }
+        });
     }
 }
